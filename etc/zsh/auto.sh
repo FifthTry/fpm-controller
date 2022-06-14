c9079b5772886cb38f2c9a09080817b484c4e0e4
wilderbit <wilderbit.net@gmail.com>
2022-06-14 13:28:25 +0530

    db setup

--- etc/zsh/auto.sh
+++ etc/zsh/auto.sh
@@ -32,3 +32,15 @@ function fmt() {
 function 0() {
   cd "$PROJDIR" || return;
 }
+
+function migrate() {
+  manage migrate $*
+}
+
+function recreatedb() {
+    psql -h 127.0.0.1 -d postgres -c "CREATE USER root;"
+    psql -h 127.0.0.1 -d postgres -c "ALTER USER root WITH SUPERUSER;"
+    psql -h 127.0.0.1 -c "DROP DATABASE IF EXISTS fpm_controller;" template1
+    psql -h 127.0.0.1 -c "CREATE DATABASE fpm_controller;" template1
+    migrate $*
+}
