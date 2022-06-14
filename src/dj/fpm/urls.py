e2254d0f9643117b29afad77d4966a0c5344a1ae
wilderbit <wilderbit.net@gmail.com>
2022-06-14 16:07:22 +0530

    remove extra

--- src/dj/fpm/urls.py
+++ src/dj/fpm/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r"dev/fpm-ready", views.fpm_ready),
-    path(r"dev/get-package", views.get_package)
+    path(r"fpm-ready", views.fpm_ready),
+    path(r"get-package", views.get_package)
 ]
