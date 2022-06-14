68d4df508932c84cfe286d2c8247fe7e12f08866
wilderbit <wilderbit.net@gmail.com>
2022-06-14 12:07:09 +0530

    pyfmt

--- src/dj/fpm/apps.py
+++ src/dj/fpm/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FpmConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fpm'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fpm"
